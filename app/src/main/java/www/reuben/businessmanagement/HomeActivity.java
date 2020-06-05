a0916f41d3079d8eef1f79d02832a885d39f3585
Reuben <gathiireuben@gmail.com>
2020-06-05 17:55:00 +0300

    adds sell button

--- app/src/main/java/www/reuben/businessmanagement/HomeActivity.java
+++ app/src/main/java/www/reuben/businessmanagement/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +17,8 @@ public class HomeActivity extends AppCompatActivity {
  @BindView(R.id.logout)
 Button btnLogout;
 FirebaseAuth mFirebaseAuth;
+@BindView(R.id.sell)
+Button btnSell;
 
 @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -31,6 +34,17 @@ FirebaseAuth mFirebaseAuth;
             startActivity(intentToMain);
         }
     });
+
+    btnSell.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(HomeActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+                Intent intentToSell = new Intent(HomeActivity.this, ProductActivity.class);
+                startActivity(intentToSell);
+            }
+    });
+
 }
 
 }
