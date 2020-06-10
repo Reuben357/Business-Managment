363fbb0ae7315518d20677acaaaf088933f27f70
Reuben <gathiireuben@gmail.com>
2020-06-10 13:20:31 +0300

    adds functionality to save and fetch data from firebase

--- app/src/main/java/www/reuben/businessmanagement/SellActivity.java
+++ app/src/main/java/www/reuben/businessmanagement/SellActivity.java
@@ -1,13 +1,70 @@
 package www.reuben.businessmanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class SellActivity extends AppCompatActivity {
+    private Button add;
+    private EditText edit;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sell);
+        setContentView(R.layout.activity_products);
+
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
+        listView = findViewById(R.id.listView);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txtproduct = edit.getText().toString();
+                if (txtproduct.isEmpty()){
+                    Toast.makeText(SellActivity.this, "There is no Product",Toast.LENGTH_SHORT).show();
+                }else {
+                    FirebaseDatabase.getInstance().getReference().child("Sold").child("Product").setValue(txtproduct);
+                }
+            }
+        });
+
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter(this,R.layout.list_item, list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Sold");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
